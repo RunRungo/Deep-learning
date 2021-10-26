5a3524d62a3e17b532055beb3b75545bf65c5f95
RunRungo <zz_seu@163.com>
2021-10-26 13:56:01 +0800

    Create LSTM_model.py

---
+++ LSTM_model.py
@@ -0,0 +1,201 @@
+
+import torch
+import torch.nn as nn
+import torchvision.transforms as transforms
+import torchvision.datasets as dsets
+from torch.autograd import Variable
+from torch.nn import Parameter
+from torch import Tensor
+import torch.nn.functional as F
+
+import math
+
+cuda = True if torch.cuda.is_available() else False
+class LSTMCell(nn.Module):
+    def __init__(self, input_size, hidden_size, bias=True):
+        super(LSTMCell, self).__init__()
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.bias = bias
+        self.x2h = nn.Linear(input_size, 4 * hidden_size, bias=bias)
+        self.h2h = nn.Linear(hidden_size, 4 * hidden_size, bias=bias)
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        std = 1.0 / math.sqrt(self.hidden_size)
+        for w in self.parameters():
+            w.data.uniform_(-std, std)
+
+    def forward(self, x, hidden):
+        hx, cx = hidden
+
+        x = x.view(-1, x.size(1))
+
+        gates = self.x2h(x) + self.h2h(hx)
+
+        gates = gates.squeeze()
+
+        ingate, forgetgate, cellgate, outgate = gates.chunk(4, 1)
+
+        ingate = F.sigmoid(ingate)
+        forgetgate = F.sigmoid(forgetgate)
+        cellgate = F.tanh(cellgate)
+        outgate = F.sigmoid(outgate)
+
+        cy = torch.mul(cx, forgetgate) + torch.mul(ingate, cellgate)
+
+        hy = torch.mul(outgate, F.tanh(cy))
+        return (hy, cy)
+
+
+'''
+STEP 3: CREATE MODEL CLASS
+'''
+
+
+class LSTMModel(nn.Module):
+    def __init__(self, input_dim, hidden_dim, layer_dim, output_dim, bias=True):
+        super(LSTMModel, self).__init__()
+
+        # Number of hidden layers
+        self.layer_dim = layer_dim
+
+        self.lstm = LSTMCell(input_dim, hidden_dim)
+
+        self.fc = nn.Linear(hidden_dim, output_dim)
+
+    def forward(self, x):
+
+
+        if torch.cuda.is_available():
+            h0 = Variable(torch.zeros(self.layer_dim, x.size(0), self.hidden_dim).cuda())
+        else:
+            h0 = Variable(torch.zeros(self.layer_dim, x.size(0), self.hidden_dim))
+
+        # Initialize cell state
+        if torch.cuda.is_available():
+            c0 = Variable(torch.zeros(self.layer_dim, x.size(0), self.hidden_dim).cuda())
+        else:
+            c0 = Variable(torch.zeros(self.layer_dim, x.size(0), self.hidden_dim))
+
+        outs = []
+
+        cn = c0[0, :, :]
+        hn = h0[0, :, :]
+
+        for seq in range(x.size(1)):
+            hn, cn = self.lstm(x[:, seq, :], (hn, cn))
+            outs.append(hn)
+
+        out = outs[-1].squeeze()
+
+        out = self.fc(out)
+        # out.size() --> 100, 10
+        return out
+
+
+'''
+STEP 4: INSTANTIATE MODEL CLASS
+'''
+input_dim = 28
+hidden_dim = 128
+layer_dim = 1  # ONLY CHANGE IS HERE FROM ONE LAYER TO TWO LAYER
+output_dim = 10
+
+model = LSTMModel(input_dim, hidden_dim, layer_dim, output_dim)
+if torch.cuda.is_available():
+    model.cuda()
+
+'''
+STEP 5: INSTANTIATE LOSS CLASS
+'''
+criterion = nn.CrossEntropyLoss()
+
+'''
+STEP 6: INSTANTIATE OPTIMIZER CLASS
+'''
+learning_rate = 0.1
+optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
+'''
+STEP
+7: TRAIN
+THE
+MODEL
+'''
+
+# Number of steps to unroll
+seq_dim = 28
+num_epochs = 1000
+loss_list = []
+iter = 0
+for epoch in range(num_epochs):
+    for i, (images, labels) in enumerate(train_loader):
+        # Load images as Variable
+        #######################
+        #  USE GPU FOR MODEL  #
+        #######################
+        if torch.cuda.is_available():
+            images = Variable(images.view(-1, seq_dim, input_dim).cuda())
+            labels = Variable(labels.cuda())
+        else:
+            images = Variable(images.view(-1, seq_dim, input_dim))
+            labels = Variable(labels)
+
+        # Clear gradients w.r.t. parameters
+        optimizer.zero_grad()
+
+        # Forward pass to get output/logits
+        # outputs.size() --> 100, 10
+        outputs = model(images)
+
+        # Calculate Loss: softmax --> cross entropy loss
+        loss = criterion(outputs, labels)
+
+        if torch.cuda.is_available():
+            loss.cuda()
+
+        # Getting gradients w.r.t. parameters
+        loss.backward()
+
+        # Updating parameters
+        optimizer.step()
+
+        loss_list.append(loss.item())
+        iter += 1
+
+        if iter % 500 == 0:
+            # Calculate Accuracy
+            correct = 0
+            total = 0
+            # Iterate through test dataset
+            for images, labels in test_loader:
+                #######################
+                #  USE GPU FOR MODEL  #
+                #######################
+                if torch.cuda.is_available():
+                    images = Variable(images.view(-1, seq_dim, input_dim).cuda())
+                else:
+                    images = Variable(images.view(-1 , seq_dim, input_dim))
+
+                # Forward pass only to get logits/output
+                outputs = model(images)
+
+                # Get predictions from the maximum value
+                _, predicted = torch.max(outputs.data, 1)
+
+                # Total number of labels
+                total += labels.size(0)
+
+                # Total correct predictions
+                #######################
+                #  USE GPU FOR MODEL  #
+                #######################
+                if torch.cuda.is_available():
+                    correct += (predicted.cpu() == labels.cpu()).sum()
+                else:
+                    correct += (predicted == labels).sum()
+
+            accuracy = 100 * correct / total
+
+            # Print Loss
+            print('Iteration: {}. Loss: {}. Accuracy: {}'.format(iter, loss.item(), accuracy))
